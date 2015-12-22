25e49260c22dca6d8b37f92ce286dac80d8317aa
chris490yy <chris_yu@epam.com>
2015-12-22 09:37:48 +0800

    TCPchat

--- js_5/client.js
+++ js_5/client.js
@@ -1,18 +1,17 @@
 
 var net = require('net');
-var color = require('colors');
 var client = new net.Socket();
 client.setEncoding('utf8');
 process.stdin.setEncoding('utf8');
 
 
 client.connect(8080, () => {
-	console.log('Connected!\n'.rainbow);	
+    console.log('Connected!\n');
 });
 
 process.stdin.pipe(client);  // user input
 
-client.on('data', function(data) {   	
-    console.log(data.red);   
+client.on('data', function(data) {
+    console.log(data);   
 });
 
--- js_5/server.js
+++ js_5/server.js
@@ -1,56 +1,84 @@
 
 
 var net = require('net');
-var color = require('colors');
 var chatServer = net.createServer();
-
 var clients = [];
 
 chatServer.on('connection', (client) => {
-	client.setEncoding('utf-8');
-	client.name = client.remotePort;
-	clients.push(client);
-	var joinMsg = client.name+' just join in the chat room!';
-	console.log(joinMsg.red);
-	client.write('You have joined in the chat room, you name is '+client.name);
-	
-	client.on('data',(data) => {
-		var chatMsg = client.name+' said: ' + data;
-        console.log(chatMsg.yellow);
-        broadcast(data,client);
-    });
+    client.setEncoding('utf-8');
+    client.name = client.remotePort;
+    clients.push(client);
+    var curTime = getTime();
+    var joinMsg = curTime + ' ' + client.name + ' just join in the chat room!';
+    console.log(joinMsg);
+    client.write(curTime + ' You have joined in the chat room, you name is ' + client.name);
 
-    client.on('end',() => {
-    	;
-    	var msg = client.name+' has left the chat room.';
-    	broadcast(msg);
+    client.on('data',(data) => {
+        data = data.slice(0,-2).toLowerCase();// remove the \n at the end of the data
+        var curTime = getTime();
+        var changeNameTag = data.slice(0,6);
+
+        if(data === '/list'){
+            var clientsName = '';
+            clients.forEach(function(element) {
+                clientsName += ' ' + element.name;
+            });
+            client.write(clientsName);
+        }
+        else if(data === '/ping') {
+            client.write('PONG');
+        }
+        else if(changeNameTag === '/name ') {
+            client.name = data.slice(6);    
+        }
+        else if(data === '/quit') {
+            client.end();
+        }
+        else{
+            var chatMsg = curTime + ' ' + client.name + ' : ' + data;
+            console.log(chatMsg);
+            broadcast(chatMsg);
+        }    
     });
 
     client.on('error',() => {
-    	var leftMsg = client.name+' just left the chat room.';
-    	console.log(leftMsg.blue);
-    	remove(client)
-    	var msg = client.name+' has left the chat room.';
-    	broadcast(msg);
+        console.log('in error');
+        clientLeft(client);
+    });
+
+    client.on('end', () => {
+        console.log('in end');
+        clientLeft(client);
     });
 });
 
-function broadcast(msg,sender) {
-	clients.forEach(function(element) {
-		if(element === sender)
-			return;
-		if(sender === undefined)
-			element.write('Server said: '+msg);
-		else{
-			element.write(sender.name+' said: '+msg);
-		}		
-	});
+function clientLeft(client) {
+    var curTime = getTime();
+    var leftMessage = curTime + ' ' + client.name + ' just left the chat room.';
+    console.log(leftMessage);
+    remove(client)
+    broadcast(leftMessage);
+}
+
+function broadcast(message) {
+    clients.forEach(function(element) {
+        element.write(message);
+    });
 }
 
 function remove(client) {
-	clients.splice(clients.indexOf(client),1);
+    clients.splice(clients.indexOf(client),1);
+}
+
+function getTime() {
+    var time = new Date();
+    var hour = time.getHours();
+    var minute = time.getMinutes();
+    var second = time.getSeconds();
+    var curTime = '['+hour+':'+minute+':'+second+']';
+    return curTime;
 }
 
 chatServer.listen(8080,() => {
-	console.log('listen to 8080');
+    console.log('listen to 8080');
 });
